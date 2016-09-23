69e9a31b37cd851cd7607e0385f08e43722e263e
Trevor Allred <trevorallred@gmail.com>
2016-09-22 22:24:09 -0700

    stable game1

---
+++ README.md
@@ -0,0 +1,10 @@
+Installing
+
+http://brysonpayne.com/2015/01/10/setting-up-pygame-on-a-mac/
+
+sudo pip3 install hg+http://bitbucket.org/pygame/pygame
+
+
+Developing the Game
+
+source venv/bin/activate
--- main.py
+++ main.py
@@ -1,26 +1,20 @@
 #! /usr/bin/env python3
 
-import os, sys
+import sys
 import pygame
-from pygame.locals import *
 from helpers import *
 
 if not pygame.font: print ('Warning, fonts disabled')
 if not pygame.mixer: print ('Warning, sound disabled')
 
+class Settings:
+    screenWidth = 800
+    screenHeight = 600
+
 class PyManMain:
-    """The Main PyMan Class - This class handles the main 
-    initialization and creating of the Game."""
-    
-    def __init__(self, width=640,height=480):
-        """Initialize"""
-        """Initialize PyGame"""
+    def __init__(self):
         pygame.init()
-        """Set the window Size"""
-        self.width = width
-        self.height = height
-        """Create the Screen"""
-        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.screen = pygame.display.set_mode((Settings.screenWidth, Settings.screenHeight))
 
 
     def MainLoop(self):
@@ -42,25 +36,21 @@ class PyManMain:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-                elif event.type == KEYDOWN:
-                    if ((event.key == K_RIGHT)
-                    or (event.key == K_LEFT)
-                    or (event.key == K_UP)
-                    or (event.key == K_DOWN)):
-                        self.snake.move(event.key)                    
+                if event.type == KEYDOWN:
+                    if event.key == K_q:
+                        sys.exit()
+                    if event.key in (K_RIGHT, K_LEFT, K_UP, K_DOWN, K_w, K_a, K_s, K_d):
+                        self.snake.move(event.key)
 
             """Check for collision"""
-            lstCols = pygame.sprite.spritecollide(self.snake
-                                                 , self.pellet_sprites
-                                                 , True)
+            lstCols = pygame.sprite.spritecollide(self.snake, self.pellet_sprites, True)
             """Update the amount of pellets eaten"""
             self.snake.pellets = self.snake.pellets + len(lstCols)
                         
             self.screen.blit(self.background, (0, 0))     
             if pygame.font:
                 font = pygame.font.Font(None, 36)
-                text = font.render("Pellets %s" % self.snake.pellets
-                                    , 1, (255, 0, 0))
+                text = font.render("Pellets %s" % self.snake.pellets, 1, (255, 0, 0))
                 textpos = text.get_rect(centerx=self.background.get_width()/2)
                 self.screen.blit(text, textpos)
                
@@ -74,12 +64,11 @@ class PyManMain:
         self.snake_sprites = pygame.sprite.RenderPlain((self.snake))
 
         """figure out how many pellets we can display"""
-        nNumHorizontal = int(self.width/64)
-        nNumVertical = int(self.height/64)       
+        nNumHorizontal = int(Settings.screenWidth/64)
+        nNumVertical = int(Settings.screenHeight/64)
         """Create the Pellet group"""
         self.pellet_sprites = pygame.sprite.Group()
-        """Create all of the pellets and add them to the 
-        pellet_sprites group"""
+        """Create all of the pellets and add them to the pellet_sprites group"""
         for x in range(nNumHorizontal):
             for y in range(nNumVertical):
                 self.pellet_sprites.add(Pellet(pygame.Rect(x*64, y*64, 64, 64)))        
@@ -87,18 +76,21 @@ class PyManMain:
 class Pellet(pygame.sprite.Sprite):
     def __init__(self, rect=None):
         pygame.sprite.Sprite.__init__(self) 
-        self.image, self.rect = load_image('pellet.png',-1)
+        self.image, self.rect = load_image('pellet2.png',-1)
         if rect != None:
             self.rect = rect
 
 class Snake(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self) 
-        self.image, self.rect = load_image('snake.png',-1)
+        self.image, self.rect = load_image('python.png',-1)
         self.pellets = 0
         """Set the number of Pixels to move each time"""
-        self.x_dist = 5
-        self.y_dist = 5
+        self.x_dist = 32
+        self.y_dist = 32
+
+        self.xMax = Settings.screenWidth - self.rect.width
+        self.yMax = Settings.screenHeight - self.rect.height
 
     def move(self, key):
         """Move your self in one of the 4 directions according to key"""
@@ -107,16 +99,20 @@ class Snake(pygame.sprite.Sprite):
         xMove = 0;
         yMove = 0;
         
-        if (key == K_RIGHT):
+        if (key == K_RIGHT or key == K_d):
             xMove = self.x_dist
-        elif (key == K_LEFT):
+        elif (key == K_LEFT or key == K_a):
             xMove = -self.x_dist
-        elif (key == K_UP):
+        elif (key == K_UP or key == K_w):
             yMove = -self.y_dist
-        elif (key == K_DOWN):
+        elif (key == K_DOWN or key == K_s):
             yMove = self.y_dist
-        self.rect.move_ip(xMove,yMove);
 
+        if self.rect.x + xMove < 0: xMove = 0
+        if self.rect.y + yMove < 0: yMove = 0
+        if self.rect.x + xMove > self.xMax: xMove = 0
+        if self.rect.y + yMove > self.yMax: yMove = 0
+        self.rect.move_ip(xMove,yMove);
 
 if __name__ == "__main__":
     MainWindow = PyManMain()
